e907c4d93bc194852ce81e20629a33da1f4678fd
mauirixxx <mauirixxx@paytonohana.net>
2018-07-30 11:35:56 -1000

    ugh forgot to rename connection

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 <BODY>
 <CENTER>
 <?php
-include_once ('connect.php');
+include_once ('connection.php');
 $con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
 session_start();
 $username = mysqli_real_escape_string($con, $_POST['username']);
