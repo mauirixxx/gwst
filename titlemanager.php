12c35bef110c39ccc282fa7669ab68544399f933
mauirixxx <mauirixx@paytonohana.net>
2018-08-06 14:22:22 -1000

    can no longer modify rank level, worded 2nd select option better

--- titlemanager.php
+++ titlemanager.php
@@ -38,7 +38,7 @@ if ($_SESSION['admin'] == 1) {
 		echo 'Modify titles <form action="titlemanager.php" method="post"><input type="hidden" name="title" value="modtitle"><select name="tid" onchange="this.form.submit()"><option selected disabled>Select title</option>';
 		include ('includes/title-select.php');
 		echo '</select><noscript><input type="submit" value="Modify Title"></noscript></form><br /><br />';
-		echo 'Add title ranks and points to <form action="titlemanager" method="post"><input type="hidden" name="title" value="addsubtitle"><select name="tid" onchange="this.form.submit()"><option selected disabled>Select title</option>';
+		echo 'Add title ranks and points to <form action="titlemanager" method="post"><input type="hidden" name="title" value="addsubtitle"><select name="tid" onchange="this.form.submit()"><option selected disabled>Add title rank(s)</option>';
         include ('includes/title-select.php');
         echo '</select><noscript><input type="submit" value="Add title rank"></noscript></form><br />';
 		echo 'Edit / Delete subtitle & points (code goes here)<br /><br />';
