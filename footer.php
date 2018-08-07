a850ae0c32489bc60a70b1c76072af8d50c4f9db
mauirixxx <mauirixx@paytonohana.net>
2018-08-06 15:24:59 -1000

    removed something stupid

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-</CENTER>
+</center>
 <?php
 // the footer just adds a logout button at the bottom of every page for the currently logged in user
 if (isset($_SESSION['userid']) && ($_SESSION['username'])) {
-	echo '<BR /><BR /><CENTER><FORM METHOD="POST" ACTION="logout.php"><INPUT TYPE="HIDDEN" NAME="logout"><INPUT TYPE="SUBMIT" VALUE="Logout"></FORM></CENTER>';
+	echo '<center><br /><br /><form method="post" action="logout.php"><input type="hidden" name="logout"><input type="submit" value="Logout"></form></center>';
 }
 ?>
-</BODY>
-</HTML>
+</body>
+</html>
