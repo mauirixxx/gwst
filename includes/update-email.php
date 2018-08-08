4bb8321d76dc9fa48a6087ea5fbc5d9994819263
mauirixxx <mauirixx@paytonohana.net>
2018-08-08 12:20:57 -1000

    changed header redirect speed I think

--- includes/update-email.php
+++ includes/update-email.php
@@ -1,11 +1,9 @@
 <?php
 include_once ('verifications.php');
-echo 'the useremail variable is: ' . $_POST['useremail'] . '<br />';
-echo 'the userid variable is: ' . $_SESSION['userid'] . '<br />';
 $updmail = $con->prepare("UPDATE userinfo SET usermail = ? WHERE userid = ?");
 $updmail->bind_param("si", $_POST['useremail'], $_SESSION['userid']);
 $updmail->execute();
 $_SESSION['usermail'] = $_POST['useremail'];
-echo 'Email address updated, redirecting.';
+echo 'E-mail address updated, redirecting.';
 header ("Refresh:2; url=preferences.php");
 ?>
