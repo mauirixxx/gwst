7019e2b5c2c30bdbe87e285c8e9310dd2343f49a
mauirixxx <mauirixx@paytonohana.net>
2018-08-10 08:25:33 -1000

    cleaned up file

--- includes/titleranks-submit.php
+++ includes/titleranks-submit.php
@@ -1,7 +1,4 @@
 <?php
-# investigate if we can put multiple input boxes and loop through them to insert into the database (see includes/titleranks-add.php)
-# https://stackoverflow.com/questions/34469482/how-to-insert-multiple-inputs-into-the-database-using-the-power-of-php
-
 $stmtstins = $con->prepare("INSERT INTO gwsubtitles (titlenameid, stname, stpoints, strank) VALUES (?, ?, ?, ?)");
 $stmtstins->bind_param("isii", $_POST['titlenameid'], $_POST['titlerankname'], $_POST['titlepoints'], $_POST['titlerank']);
 $stmtstins->execute();
