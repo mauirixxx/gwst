ab2f1169dc41212d601e8d3539ca9af2ed54993d
mauirixxx <mauirixx@paytonohana.net>
2018-08-09 15:32:36 -1000

    fixed undefined variable index error popping up when nobody is logged in

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 </center>
 <?php
 echo '<hr>';
-echo '<center>| Preferred game account: <b>' . $_SESSION['prefaccname'] . '</b> | Preferred character: <b>' . $_SESSION['prefcharname'] . '</b> |</center><br />';
+if (isset($_SESSION['prefaccname']) && ($_SESSION['prefcharname'])) {
+	echo '<center>| Currently selected game account: <b>' . $_SESSION['prefaccname'] . '</b> | Current character: <b>' . $_SESSION['prefcharname'] . '</b> |</center><br />';
+}
 // the footer just adds a logout button at the bottom of every page for the currently logged in user
 if (isset($_SESSION['userid']) && ($_SESSION['username'])) {
 	echo '<center><br /><br /><form method="post" action="logout.php"><input type="hidden" name="action" value="logout" ><input type="submit" value="Logout"></form></center>';
