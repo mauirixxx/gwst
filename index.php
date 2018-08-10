7c2e5f38f925e8783b463c306f6c4bf43a9564de
mauirixxx <mauirixx@paytonohana.net>
2018-08-09 15:33:51 -1000

    re-ordered the links I think?

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 $pagetitle = "Guild Wars Stats Tracker";
 include_once ('header.php');
 if (isset($_SESSION['userid'])){
+	echo 'View individual character stats <A HREF="listchars.php" CLASS="navlink">here</A><BR /><BR />'; //make this a drop down list later
 	echo 'Update character stats <A HREF="updatecharstats.php" CLASS="navlink">here</A><BR /><BR />';
 	echo 'Update account stats <A HREF="updateaccountstats.php" CLASS="navlink">here</A><BR /><BR />';
-	echo 'View individual character stats <A HREF="listchars.php" CLASS="navlink">here</A><BR /><BR />'; //make this a drop down list later
-    echo 'Add Guild Wars account <a href="addaccounts.php" class="navlink">here</a><br />';
+	echo 'Add Guild Wars accounts and characters <a href="addaccounts.php" class="navlink">here</a><br />';
 }
 include_once ('footer.php');
 ?>
