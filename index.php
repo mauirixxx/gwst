4fc4ab4bca9cd579203268831e1c67af85a3719d
mauirixxx <mauirixx@paytonohana.net>
2018-08-09 22:11:48 -1000

    added link to character creation page

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 $pagetitle = "Guild Wars Stats Tracker";
 include_once ('header.php');
 if (isset($_SESSION['userid'])){
-	echo 'View individual character stats <A HREF="listchars.php" CLASS="navlink">here</A><BR /><BR />'; //make this a drop down list later
-	echo 'Update character stats <A HREF="updatecharstats.php" CLASS="navlink">here</A><BR /><BR />';
-	echo 'Update account stats <A HREF="updateaccountstats.php" CLASS="navlink">here</A><BR /><BR />';
-	echo 'Add Guild Wars accounts and characters <a href="addaccounts.php" class="navlink">here</a><br />';
+	echo 'View individual character stats <A HREF="listchars.php" class="navlink">here</A><BR /><BR />'; //make this a drop down list later
+	echo 'Update character stats <A HREF="updatecharstats.php" class="navlink">here</A><BR /><BR />';
+	echo 'Update account stats <A HREF="updateaccountstats.php" class="navlink">here</A><BR /><BR />';
+	echo 'Add Guild Wars accounts and manage characters <a href="addaccounts.php" class="navlink">here</a><br /><br />';
+    echo 'Add a <a href="addcharacters.php" class="navlink">new character</a> to track<br />';
 }
 include_once ('footer.php');
 ?>
