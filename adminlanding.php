ef3345aed8d95955fbcb215d12a87bcf43630ff4
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-21 08:32:07 -1000

    Remove obsolete autofilled title test link

--- adminlanding.php
+++ adminlanding.php
@@ -14,7 +14,6 @@ unset($_SESSION['tid']);
 echo '<center>Welcome to the admin area!<br /><br />';
 echo 'Title creator / editor <a href="titlemanager.php" class="navlink">here</a> (work in progress)<br /><br />';
 echo 'User editor <a href="" class="navlink">here</a> (not working yet)<br /><br />';
-echo 'testing autofilled title theories: <a href="autofilled.php" class="navlink">Legendary title testGWAMM</a><br /><br />';
 
 include_once ('footer.php');
 ?>
