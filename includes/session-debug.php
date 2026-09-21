ddb5b34c60c6759774f58bb6deeeb9d4047a415e
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-21 02:35:44 -1000

    Remove obsolete session debug helper

--- includes/session-debug.php
+++
@@ -1,7 +0,0 @@
-<?php
-# delete this block when shit finally works.
-ini_set('display_errors', 'on');
-error_reporting(E_ALL);
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-# delete the above when shit finally works
-?>
