c256fb46b5886061cb394d31ce0d98400e3acb2a
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-20 22:19:17 -1000

    Harden login and session handling

--- logout.php
+++ logout.php
@@ -1,17 +1,28 @@
 <?php
-$pagetitle = "Logging Out";
-include_once ('header.php');
-if (isset($_GET['action'])) {
-	$logout = $_GET['action'];
-} else {
-	$logout = $_POST['action'];
+if (session_status() == PHP_SESSION_NONE) {
+	ini_set('session.use_strict_mode', '1');
+	ini_set('session.use_only_cookies', '1');
+	ini_set('session.cookie_httponly', '1');
+	ini_set('session.cookie_samesite', 'Lax');
+	if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+		ini_set('session.cookie_secure', '1');
+	}
+	session_start();
 }
-if ($logout == "logout"){
-	session_unset();
+
+if (isset($_GET['action']) && $_GET['action'] === 'logout') {
+	$_SESSION = array();
+
+	if (ini_get("session.use_cookies")) {
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+	}
+
 	session_destroy();
-	header("refresh:2;url=index.php");
-	echo '<CENTER>You have been logged out ...<BR />Returning to login screen in a few seconds</CENTER>';
-} else {
-	echo '<CENTER>Something went wrong, you haven\'t been logged out!<BR /><BR />Please click <A HREF="logout.php" CLASS="navlink">HERE</A> to try again</CENTER>';
+	header("Location: index.php");
+	exit();
 }
+
+http_response_code(400);
+echo '<CENTER>Something went wrong, you haven\'t been logged out!<BR /><BR />Please return to <A HREF="index.php" CLASS="navlink">GWST</A>.</CENTER>';
 ?>
