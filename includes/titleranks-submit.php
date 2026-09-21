a083d0fbd7f09a8bd378024de9b066964b5e3664
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-21 02:11:06 -1000

    Improve title rank submission feedback

--- includes/titleranks-submit.php
+++ includes/titleranks-submit.php
@@ -7,7 +7,13 @@ if (isset($_SESSION['userid']) && isset($_SESSION['admin']) && $_SESSION['admin'
     $_SESSION['title'] = "repeat";
     $_SESSION['tid'] = $_POST['titlenameid'];
     $_SESSION['tr'] = $_POST['titlerank'];
-    echo 'Title rank added, redirecting!';
+    $stmtname = $con->prepare("SELECT titlename FROM gwtitles WHERE titlenameid = ?");
+    $stmtname->bind_param("i", $_POST['titlenameid']);
+    $stmtname->execute();
+    $stmtname->bind_result($title_name);
+    $stmtname->fetch();
+    $stmtname->close();
+    echo 'Added rank <b>' . h($_POST['titlerankname']) . '</b> to <b>' . h($title_name) . '</b>, redirecting!';
     header ("Refresh:1; url=titlemanager.php");
 }
 ?>
