c4c818d9da0bc64d48ff3809d98dd408c048d46e
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-20 23:03:02 -1000

    Harden admin title actions

--- includes/title-submit.php
+++ includes/title-submit.php
@@ -1,10 +1,10 @@
 <?php
-if (isset($_SESSION['userid'])) {
+if (isset($_SESSION['userid']) && isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
 	if (!isset($_POST['autofill'])) {
-		$_POST['autofill'] == 0;
+		$_POST['autofill'] = 0;
 	}
 	if (!isset($_POST['gwamm'])) {
-			$_POST['gwamm'] == 0;
+			$_POST['gwamm'] = 0;
 	} else {
 		// $ggid = Get Gwamm ID
 		$ggid = $con->prepare("SELECT titlenameid FROM gwtitles WHERE gwamm = 1");
