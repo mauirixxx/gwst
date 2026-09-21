6cfd3ffeeaa56b8194020d55763607dbf5c4c838
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-20 22:57:12 -1000

    Harden registration handling

--- register.php
+++ register.php
@@ -4,28 +4,51 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 <?php
 if (session_status() == PHP_SESSION_NONE) {
-	session_start();
+    ini_set('session.use_strict_mode', '1');
+    ini_set('session.use_only_cookies', '1');
+    ini_set('session.cookie_httponly', '1');
+    ini_set('session.cookie_samesite', 'Lax');
+    if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+        ini_set('session.cookie_secure', '1');
+    }
+    session_start();
 }
+include_once (__DIR__ . '/includes/html.php');
+include_once (__DIR__ . '/includes/csrf.php');
 include_once ('connect.php');
 $con = mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
 if ($con->connect_errno){
-	die ('Unable to connect to database [' . $db->connect_errno . ']');
+    die ('Unable to connect to database [' . $con->connect_errno . ']');
 }
+
 if (empty($_POST['reguser'])){
-	// this block contains the code to fill out the form
-	echo '<center>';
-	include_once ('includes/account-registration.php');
-	echo '</center><br />';
+    echo '<center>';
+    include_once ('includes/account-registration.php');
+    echo '</center><br />';
 } else {
-	include_once ('includes/verifications.php');
-	// this block validates input, and if passed, inserts it into the database
-	$hashedpass = password_hash($_POST['userpass1'], PASSWORD_DEFAULT);
-	$stmt = $con->prepare("INSERT INTO userinfo (username, userpass, usermail) VALUES (?, ?, ?)");
-	$stmt->bind_param("sss", $username, $hashedpass, $verifyemail);
-	$stmt->execute();
-	echo '<center>Account created, please <a href="index.php">login</a> to continue<br /><br />';
-	session_destroy();
-	exit();
+    csrf_require_valid_post();
+
+    if (!isset($_POST['username'], $_POST['useremail'], $_POST['userpass1'], $_POST['userpass2'])
+        || trim($_POST['username']) === ''
+        || trim($_POST['useremail']) === ''
+        || $_POST['userpass1'] === ''
+        || $_POST['userpass2'] === '') {
+        http_response_code(400);
+        echo '<center>All registration fields are required.<br /><a href="register.php" class="navlink">Please try again!</a><br />';
+        exit();
+    }
+
+    include_once ('includes/verifications.php');
+    $hashedpass = password_hash($_POST['userpass1'], PASSWORD_DEFAULT);
+    $stmt = $con->prepare("INSERT INTO userinfo (username, userpass, usermail) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $hashedpass, $verifyemail);
+    $stmt->execute();
+    $stmt->close();
+
+    echo '<center>Account created, please <a href="index.php">login</a> to continue<br /><br />';
+    $_SESSION = array();
+    session_destroy();
+    exit();
 }
 echo '<center>Back to <a href="index.php" class="navlink">home page</a><br />';
 include_once ('footer.php');
