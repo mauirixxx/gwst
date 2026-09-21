4b3980bac7d670593a40ba975b0b001cda716588
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-21 02:35:36 -1000

    Harden account preference selection

--- includes/set-prefacc.php
+++ includes/set-prefacc.php
@@ -1,6 +1,6 @@
 <?php
-if (isset($_SESSION['userid'])) {
-	if ($_POST['prefaccid'] == "nopref") {
+if (isset($_SESSION['userid']) && isset($_POST['prefaccid'])) {
+	if ($_POST['prefaccid'] === "nopref") {
 		// $nap = No AccountID Preferrence
 		$nap = $con->prepare("UPDATE userinfo SET prefaccid = 0, prefaccname = 'No default selected' WHERE userid = ?");
 		$nap->bind_param("i", $_SESSION['userid']);
@@ -10,18 +10,27 @@ if (isset($_SESSION['userid'])) {
 		$_SESSION['prefaccname'] = "No default selected";
 		echo 'Account preference update - no preferred account selected.<br />';
 	} else {
+		$prefaccid = filter_var($_POST['prefaccid'], FILTER_VALIDATE_INT);
+		if ($prefaccid === false || $prefaccid < 1) {
+			echo 'Invalid account preference.<br />';
+			return;
+		}
 		// $sap = Select AccountID Preferrence
 		$sap = $con->prepare("SELECT accid, accemail FROM gwaccounts WHERE accid = ? AND userid = ?");
-		$sap->bind_param("ii", $_POST['prefaccid'], $_SESSION['userid']);
+		$sap->bind_param("ii", $prefaccid, $_SESSION['userid']);
 		$sap->execute();
 		$result = $sap->get_result();
-		while ($row = $result->fetch_assoc()) {
+		if ($row = $result->fetch_assoc()) {
 			$uap = $con->prepare("UPDATE userinfo SET prefaccid = ?, prefaccname = ? WHERE userid = ?");
-			$uap->bind_param("isi", $_POST['prefaccid'], $row['accemail'], $_SESSION['userid']);
+			$uap->bind_param("isi", $prefaccid, $row['accemail'], $_SESSION['userid']);
 			$uap->execute();
 			$uap->close();
 			$_SESSION['prefaccid'] = $row['accid'];
 			$_SESSION['prefaccname'] = $row['accemail'];
+		} else {
+			echo 'Account preference not found.<br />';
+			$sap->close();
+			return;
 		}
 		$sap->close();
 	}
