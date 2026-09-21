dc997b1d763adee4c07a1d3e0db4e6ba2e8565f6
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-21 02:35:38 -1000

    Harden character preference selection

--- includes/set-prefchar.php
+++ includes/set-prefchar.php
@@ -1,6 +1,6 @@
 <?php
-if (isset($_SESSION['userid'])) {
-    if ($_POST['prefcharid'] == "nopref") {
+if (isset($_SESSION['userid']) && isset($_POST['prefcharid'])) {
+    if ($_POST['prefcharid'] === "nopref") {
 		// $ncp = No CharID Preferrence
         $ncp = $con->prepare("UPDATE userinfo SET prefcharid = 0, prefcharname = 'No default selected' WHERE userid = ?");
 		$ncp->bind_param("i", $_SESSION['userid']);
@@ -10,20 +10,28 @@ if (isset($_SESSION['userid'])) {
 		$_SESSION['prefcharname'] = "No default selected";
 		$_SESSION['charprofid'] = "0";
 	} else {
+		$prefcharid = filter_var($_POST['prefcharid'], FILTER_VALIDATE_INT);
+		if ($prefcharid === false || $prefcharid < 1) {
+			echo 'Invalid character preference.<br />';
+			return;
+		}
 		// $scp = Selected CharID Preferrence
 		$scp = $con->prepare("SELECT charid, charname, profid FROM gwchars WHERE charid = ? AND accid = ? AND userid = ?");
-		$scp->bind_param("iii", $_POST['prefcharid'], $_SESSION['prefaccid'], $_SESSION['userid']);
+		$scp->bind_param("iii", $prefcharid, $_SESSION['prefaccid'], $_SESSION['userid']);
 		$scp->execute();
 		$result = $scp->get_result();
-		while ($row = $result->fetch_assoc()) {
+		if ($row = $result->fetch_assoc()) {
 			$uap = $con->prepare("UPDATE userinfo SET prefcharid = ?, prefcharname = ? WHERE userid = ?");
-			$uap->bind_param("isi", $_POST['prefcharid'], $row['charname'], $_SESSION['userid']);
+			$uap->bind_param("isi", $prefcharid, $row['charname'], $_SESSION['userid']);
 			$uap->execute();
 			$uap->close();
 			$_SESSION['prefcharid'] = $row['charid'];
 			$_SESSION['prefcharname'] = $row['charname'];
 			$_SESSION['charprofid'] = $row['profid'];
+		} else {
+			echo 'Character preference not found.<br />';
 		}
+		$scp->close();
 	}
 }
 ?>
