bc5c58009824536d50d726d9911522bf3d2f45d0
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-20 22:07:08 -1000

    Harden admin page authorization

--- adminlanding.php
+++ adminlanding.php
@@ -1,14 +1,20 @@
 <?php
 $pagetitle = "Admin Area";
 include_once ('header.php');
-if (isset($_SESSION['userid'])){
-	unset($_SESSION['title']);
-	unset($_SESSION['tid']);
-	echo '<center>Welcome to the admin area!<br /><br />';
-	echo 'Title creator / editor <a href="titlemanager.php" class="navlink">here</a> (work in progress)<br /><br />';
-	echo 'User editor <a href="" class="navlink">here</a> (not working yet)<br /><br />';
-	echo 'testing autofilled title theories: <a href="autofilled.php" class="navlink">Legendary title testGWAMM</a><br /><br />';
-	include_once ('includes/session-dump.php');
+
+if (!isset($_SESSION['userid']) || !isset($_SESSION['admin']) || $_SESSION['admin'] != 1) {
+	http_response_code(403);
+	echo '<center>Access denied.</center>';
+	include_once ('footer.php');
+	exit();
 }
+
+unset($_SESSION['title']);
+unset($_SESSION['tid']);
+echo '<center>Welcome to the admin area!<br /><br />';
+echo 'Title creator / editor <a href="titlemanager.php" class="navlink">here</a> (work in progress)<br /><br />';
+echo 'User editor <a href="" class="navlink">here</a> (not working yet)<br /><br />';
+echo 'testing autofilled title theories: <a href="autofilled.php" class="navlink">Legendary title testGWAMM</a><br /><br />';
+
 include_once ('footer.php');
 ?>
