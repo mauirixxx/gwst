9b0a229942b71b9f9afc2cbd5a279e4daa0abb1c
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-20 22:27:14 -1000

    Add CSRF protection

--- footer.php
+++ footer.php
@@ -8,6 +8,24 @@ if (isset($_SESSION['prefaccname']) && ($_SESSION['prefcharname'])) {
 if (isset($_SESSION['userid']) && ($_SESSION['username'])) {
 	echo '<center><br /><br /><form method="post" action="logout.php"><input type="hidden" name="action" value="logout" ><input type="submit" value="Logout"></form></center>';
 }
+if (isset($_SESSION['userid'])) {
+	$csrf_token_json = json_encode(csrf_token());
+	echo '<script>
+	(function () {
+		const token = ' . $csrf_token_json . ';
+		document.querySelectorAll("form").forEach(function (form) {
+			if ((form.method || "").toLowerCase() !== "post" || form.querySelector("input[name=csrf_token]")) {
+				return;
+			}
+			const input = document.createElement("input");
+			input.type = "hidden";
+			input.name = "csrf_token";
+			input.value = token;
+			form.appendChild(input);
+		});
+	})();
+	</script>';
+}
 ?>
 </body>
 </html>
