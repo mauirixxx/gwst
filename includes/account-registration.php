56aa5db96fa8224859c4a732ea3a83622d6f9d0c
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-20 22:57:15 -1000

    Protect registration form with CSRF

--- includes/account-registration.php
+++ includes/account-registration.php
@@ -3,7 +3,7 @@
 <tr><th colspan="2">Username desired</th></tr>
 <tr><td colspan="2"><input type="text" size="45" required="required" name="username"></td></tr>
 <tr><th colspan="2">E-Mail address</th></tr>
-<tr><td colspan="2"><input type="text" size="45" required="required" name="useremail"></td></tr>
+<tr><td colspan="2"><input type="email" size="45" required="required" name="useremail"></td></tr>
 <tr><th>Password</th><th>Verify password</th></tr>
 <tr><td><input type="password" required="required" name="userpass1" id="up1"></td><td><input type="password" required="required" name="userpass2" id="up2"></td></tr>
 </table>
@@ -18,6 +18,7 @@
         return true;
     }
 </script>
+<input type="hidden" name="csrf_token" value="<?php echo h(csrf_token()); ?>">
 <input type="hidden" name="reguser" value="1">
 <input type="submit" name="submission" value="Go! Go! Go!" onclick="return Validate()" id="btnSubmit">
 </form>
