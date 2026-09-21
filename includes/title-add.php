b5aa1ae92f504ae5692a1bc1fae538accb073eed
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-21 02:10:52 -1000

    Improve title manager feedback

--- includes/title-add.php
+++ includes/title-add.php
@@ -1,5 +1,6 @@
 <?php
 if (isset($_SESSION['userid']) && isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
+    echo '<b>Adding a new title</b><br /><br />';
     echo '<form action="titlemanager.php" method="post"><table border="1"><tr><th>Title Name</th><th>Title Type</th><th>Max Rank</th><th>Auto filled?</th><td>GWAMM?</td></tr>';
     echo '<tr><td><input type="text" name="titlename" placeholder="Friend of the Kurzicks" required autofocus></td><td style="text-align:left"><input type="radio" name="titletype" value="0" checked> Account<br /><input type="radio" name="titletype" value="1">Character</td>';
     echo '<td><input type="number" name="titlemaxrank" min="0" max="15"></td><td><input type="checkbox" name="autofill" value="1"></td><td><input type="checkbox" name="gwamm" value="1"></td></tr>';
