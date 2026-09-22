c17ad283beb9745a2dc54ba6f7965a4d15b63759
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-21 17:20:15 -1000

    Fix character header loader on password reset branch

--- header-list-chars.php
+++ header-list-chars.php
@@ -1,13 +1,18 @@
 <?php
-if (isset($_SESSION['userid'])) {
-	// $cls = Character List Select
-	$cls = $con->prepare("SELECT charid, charname, profid FROM gwchars WHERE accid = ? AND userid = ? ORDER BY charname");
-	$cls->bind_param("ii", $_SESSION['prefaccid'], $_SESSION['userid']);
-	$cls->execute();
-	$clsres = $cls->get_result();
-	while ($clsrow = $clsres->fetch_assoc()) {
-		echo '<option class="profession-' . $clsrow['profid'] . '" value="' . $clsrow['charid'] . '">' . h($clsrow['charname']) . '</option>';
-	}
-	$cls->close();
+
+$header_characters = array();
+
+if (isset($_SESSION['userid']) && !empty($_SESSION['prefaccid'])) {
+    // $cls = Character List Select
+    $cls = $con->prepare("SELECT charid, charname, profid FROM gwchars WHERE accid = ? AND userid = ? ORDER BY charname");
+    $cls->bind_param("ii", $_SESSION['prefaccid'], $_SESSION['userid']);
+    $cls->execute();
+    $clsres = $cls->get_result();
+
+    while ($clsrow = $clsres->fetch_assoc()) {
+        $header_characters[] = $clsrow;
+    }
+
+    $cls->close();
 }
 ?>
