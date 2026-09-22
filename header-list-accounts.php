8808c4c859e6d40dc5942d4f2f38378f55bcf8db
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-21 17:10:13 -1000

    Fix account list header data loader

--- header-list-accounts.php
+++ header-list-accounts.php
@@ -1,13 +1,18 @@
 <?php
+
+$header_accounts = array();
+
 if (isset($_SESSION['userid'])) {
     // $als = Account List Select
-    $als = $con->prepare("SELECT accid, accemail FROM gwaccounts WHERE userid = ?");
+    $als = $con->prepare("SELECT accid, accemail FROM gwaccounts WHERE userid = ? ORDER BY accemail");
     $als->bind_param("i", $_SESSION['userid']);
     $als->execute();
     $alsres = $als->get_result();
+
     while ($alsrow = $alsres->fetch_assoc()) {
-        echo '<option value="' . $alsrow['accid'] . '">' . h($alsrow['accemail']) . '</option>';
+        $header_accounts[] = $alsrow;
     }
+
     $als->close();
 }
 ?>
