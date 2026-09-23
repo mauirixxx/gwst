714b5b2d78d543716a857e93d2c1542bbf5bdde3
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-22 19:12:35 -1000

    Enforce registration field limits in form

--- includes/account-registration.php
+++ includes/account-registration.php
@@ -1,22 +1,22 @@
 <form action="register.php" method="post" class="auth-form" id="registration-form">
     <div class="auth-field">
         <label for="register-username">Username</label>
-        <input id="register-username" type="text" name="username" autocomplete="username" maxlength="50" required autofocus>
+        <input id="register-username" type="text" name="username" autocomplete="username" minlength="1" maxlength="30" pattern="[A-Za-z0-9]+" title="Use letters and numbers only, with no spaces." required autofocus>
     </div>
 
     <div class="auth-field">
         <label for="register-email">E-mail address</label>
-        <input id="register-email" type="email" name="useremail" autocomplete="email" required>
+        <input id="register-email" type="email" name="useremail" autocomplete="email" maxlength="50" required>
     </div>
 
     <div class="auth-field">
         <label for="up1">Password</label>
-        <input id="up1" type="password" name="userpass1" autocomplete="new-password" required>
+        <input id="up1" type="password" name="userpass1" autocomplete="new-password" minlength="8" maxlength="255" required>
     </div>
 
     <div class="auth-field">
         <label for="up2">Verify password</label>
-        <input id="up2" type="password" name="userpass2" autocomplete="new-password" required>
+        <input id="up2" type="password" name="userpass2" autocomplete="new-password" minlength="8" maxlength="255" required>
     </div>
 
     <input type="hidden" name="csrf_token" value="<?php echo h(csrf_token()); ?>">
