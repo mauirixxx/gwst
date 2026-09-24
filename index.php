ad3cce15f176bcb94d3da0ee9b53ab6b092f2845
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-23 23:43:58 -1000

    Update home page title for GWTTT rename

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- this is the main directory of the site, which links to the various content pages -->
 <?php
-$pagetitle = "Guild Wars Stats Tracker";
+$pagetitle = "Guild Wars Titles & Treasures Tracker";
 include_once ('header.php');
 if (isset($_SESSION['userid'])){
 	include_once ('includes/current-character.php');
